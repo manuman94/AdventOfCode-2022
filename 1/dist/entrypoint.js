3de1d0875e4f37b361d1e5f6d224ee497f31832f
Blasco Galdón, Manu <jose.blasco@oracle.com>
2023-01-25 09:01:42 +0100

    Removed ignored dist directory

--- 1/dist/entrypoint.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const CalorieCounting_1 = require("./src/application/CalorieCounting");
-CalorieCounting_1.CalorieCounting.start();
